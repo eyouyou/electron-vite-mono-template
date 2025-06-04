745eb233bec2b56a3f27a0c1c2083b7e0082e8f8
eyouyou <393370459@qq.com>
2025-06-04 20:58:46 +0800

    增加各种调试支持

--- dev.ts
+++ dev.ts
@@ -11,16 +11,26 @@ const outDir = path.resolve(__dirname, './dist')
 let electronProcess: ChildProcess | null = null
 
 async function startRenderer() {
-  const server = await createServer({
-    configFile: path.resolve(__dirname, 'packages', 'renderer', 'vite.config.ts'),
-    server: {
-      port: 5173, // 确保端口与 renderer 的配置一致
-      strictPort: true, // 确保端口不可用时抛出错误
-      hmr: true, // 启用热模块替换
-    }
-  })
-  await server.listen()
-  console.log('[vite] renderer dev server running...')
+  try {
+    const rendererPath = path.resolve(__dirname, 'packages', 'renderer')
+    const server = await createServer({
+      configFile: path.resolve(rendererPath, 'vite.config.ts'),
+      root: rendererPath,
+      server: {
+        port: 5173, // 确保端口与 renderer 的配置一致
+        strictPort: true, // 确保端口不可用时抛出错误
+        hmr: true, // 启用热模块替换
+      }
+    })
+    var s = await server.listen()
+    server.printUrls()
+    console.log('✅ Vite 开发服务已启动')
+    return server
+  } catch (error) {
+    console.error('❌ 启动 Vite 开发服务失败:', error)
+    process.exit(1) // 退出进程
+  }
+
 }
 
 async function ensureDirExists(dirPath: string) {
@@ -33,25 +43,54 @@ async function ensureDirExists(dirPath: string) {
   }
 }
 
-async function buildMainAndWatch() {
-  await ensureDirExists(outDir);
+async function emptyOutDir(dirPath: string) {
+  const absPath = path.resolve(dirPath)
+  try {
+    await fs.access(absPath)
+    await fs.rm(absPath, { recursive: true, force: true })
+    console.log(`✅ 已清空目录: ${absPath}`)
+  }
+  catch (error) {
+    console.error(`❌ 清空目录失败: ${absPath}`, error)
+  }
+
+}
+
+async function buildMain() {
   const result = await build({
     build: {
       lib: {
-        entry: path.resolve('packages', 'main', 'src/main.ts'),
+        entry: path.resolve('packages', 'main', 'src/index.ts'),
         formats: ['cjs'],
         fileName: () => 'main.js',
       },
+      sourcemap: true,
       outDir: outDir,
+      emptyOutDir: false,
       target: 'node22',
       rollupOptions: {
         external: ['electron', 'fs', 'path'],
       },
     },
   })
-  // 开始监听打包输出
-  watch(outDir, { recursive: true }, () => {
-    restartElectron()
+}
+
+async function buildPreload() {
+  const result = await build({
+    build: {
+      lib: {
+        entry: path.resolve('packages', 'preload', 'src/index.ts'),
+        formats: ['cjs'],
+        fileName: () => 'preload.js',
+      },
+      sourcemap: true,
+      outDir: outDir,
+      target: 'node22',
+      emptyOutDir: false,
+      rollupOptions: {
+        external: ['electron', 'fs', 'path'],
+      },
+    },
   })
 }
 
@@ -61,12 +100,12 @@ function restartElectron() {
     electronProcess = null
   }
 
-  spawn(electronPath as unknown as string, [path.resolve(outDir, './main.js')], {
+  spawn(electronPath as unknown as string, [path.resolve(outDir, './main.js'), '--remote-debugging-port=9222'], {
     stdio: 'inherit', // 或 'pipe' 用于监听子进程输出
     env: {
       ...process.env,
       NODE_ENV: 'development',
-      ELECTRON_RENDERER_URL: 'http://localhost:5173' // 确保 renderer 的 URL 正确
+      ELECTRON_RENDERER_URL: 'http://localhost:5173', // 确保 renderer 的 URL 正确
     }
   });
 
@@ -74,9 +113,40 @@ function restartElectron() {
 }
 
 async function main() {
-  await startRenderer()
-  await buildMainAndWatch()
+  await emptyOutDir(outDir)
+  var server = await startRenderer()
+  if (!server) {
+    console.error('❌ Vite renderer server failed to start.')
+    return
+  }
+  console.log('[vite] renderer dev server running...')
+  await ensureDirExists(outDir);
+  console.log('[vite] output directory ensured:', outDir);
+  await buildPreload()
+  console.log('[vite] preload built successfully.')
+  console.log('[vite] main process build started...')
+  await buildMain()
+
+  // 开始监听打包输出
+  watch(outDir, { recursive: true }, () => {
+    restartElectron()
+  })
+  console.log('[vite] main process watching for changes...')
+
   restartElectron()
+  console.log('[electron] main process started.')
+
+  process.on('SIGINT', async () => {
+    console.log('🛑 收到 SIGINT，正在关闭 Vite 服务...')
+    await server.close()
+    process.exit(0)
+  })
+
+  process.on('SIGTERM', async () => {
+    console.log('🛑 收到 SIGTERM，正在关闭 Vite 服务...')
+    await server.close()
+    process.exit(0)
+  })
 }
 
 main()
